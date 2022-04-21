067d5995013db988363116f92f84a8d2992201d3
Nantonisse <noel.antonisse@kellogg.northwestern.edu>
2022-04-21 00:28:29 +0000

    1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,48 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 5
+puts 3
 
 # Perform simple math with numbers
+puts 5 + 3
+puts 5*3
+puts 5 / 3
+
+#modulus operator (remainder)
+puts 5%3
+
+#get decimal values
+puts 5.0/3.0
 
 # Strings
+puts "hello, world"
+
 
 # Combine strings together
+puts "hello " + "world"
+puts "tacos" * 3
+#can't do 3 * "tacos" or "Tacos" + 3
+
+#convert 3 to a string
+puts "tacos" +3.to_s
 
 # Variables
+a = 5
+b = 3
+puts a * b
 
+
+food = "tacos"
+quantity = 3
+puts food + ": " + quantity.to_s
 # Combine strings and variables
+puts "#{food}: #{quantity}"
+first_name = "Mando"
+puts "Hello, #{first_name}"
+puts "Hello, " + first_name
+
 
 # String manipulation
+puts "Hello".reverse
+puts "Hello".downcase
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -2,9 +2,17 @@
 # is the same as where this file is located and then run:
 # ruby 2-conditionals.rb
 
+
 # Booleans
+this_is_true = true
+this_is_false = false 
+puts this_is_true
+puts this_is_false
 
 # Boolean Expressions
+#check if true or false
+puts 3 == 2
+puts 3 <= 2
 
 # If Conditional Logic
 
